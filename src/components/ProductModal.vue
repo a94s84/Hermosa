3c45e8e0fe8f5224b64ce10a84882af9d66bc5c6
Stacy <a94s84@gmail.com>
2022-03-03 17:30:42 +0800

    產品資料更新.上傳圖片api

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -7,23 +7,94 @@
     aria-hidden="true"
     ref="modal"
     >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>新增產品</span>
+          </h5>
+          <button type="button" class="btn-close"
+                  data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="mb-3">
+                <label for="image" class="form-label">輸入圖片網址</label>
+                <input type="text" class="form-control" id="image"
+                  v-model="innerTempProduct.imageUrl" placeholder="請輸入圖片連結">
+              </div>
+              <div class="mb-3">
+                <label for="customFile" class="form-label">或 上傳圖片
+                  <i class="fas fa-spinner fa-spin"></i>
+                </label>
+                <input type="file" id="customFile" class="form-control"  ref="fileInput" @change="uploadFile">
+              </div>
+              <img class="img-fluid" :src="innerTempProduct.imageUrl" alt="">
+              <!-- 延伸技巧，多圖 -->
+            </div>
+            <div class="col-sm-8">
+              <div class="mb-3">
+                <label for="title" class="form-label">*標題</label>
+                <input type="text" class="form-control" id="title" v-model="innerTempProduct.title" required
+                        placeholder="請輸入標題">
+              </div>
+              <div class="row gx-2">
+                <div class="mb-3 col-md-6">
+                  <label for="category" class="form-label">*分類</label>
+                  <input type="text" class="form-control" id="category" v-model="innerTempProduct.category" required
+                          placeholder="請輸入分類">
+                </div>
+                <div class="mb-3 col-md-6">
+                  <label for="price" class="form-label">*單位</label>
+                  <input type="text" class="form-control" id="unit" required
+                    v-model="innerTempProduct.unit"  placeholder="請輸入單位">
+                </div>
+              </div>
+
+              <div class="row gx-2">
+                <div class="mb-3 col-md-6">
+                  <label for="origin_price" class="form-label">*原價</label>
+                  <input type="number" class="form-control" id="origin_price" required
+                   v-model="innerTempProduct.origin_price" placeholder="請輸入原價">
+                </div>
+                <div class="mb-3 col-md-6">
+                  <label for="price" class="form-label">*售價</label>
+                  <input type="number" class="form-control" id="price" required
+                    v-model="innerTempProduct.price" placeholder="請輸入售價">
+                </div>
+              </div>
+              <hr>
+
+              <div class="mb-3">
+                <label for="description" class="form-label">產品描述</label>
+                <textarea type="text" class="form-control" id="description"
+                 v-model="innerTempProduct.description" placeholder="請輸入產品描述"></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="content" class="form-label">說明內容</label>
+                <textarea type="text" class="form-control" id="content"
+                 v-model="innerTempProduct.content" placeholder="請輸入產品說明內容" rows="5"></textarea>
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox"
+                          :true-value="1"
+                          :false-value="0"
+                          id="is_enabled"  v-model="innerTempProduct.is_enabled">
+                  <label class="form-check-label" for="is_enabled">
+                    是否啟用
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-outline-secondary"
+                  data-bs-dismiss="modal">取消
+          </button>
+          <button type="button" class="btn btn-primary" @click="$emit('update-product',innerTempProduct)">確認</button>
         </div>
       </div>
     </div>
@@ -34,9 +105,21 @@
 import Modal from 'bootstrap/js/dist/modal'
 
 export default {
+  props: {
+    modalProduct: {
+      type: Object,
+      default () { return {} }
+    }
+  },
+  watch: {
+    modalProduct () {
+      this.innerTempProduct = this.modalProduct
+    }
+  },
   data () {
     return {
-      modal: {}
+      modal: {},
+      innerTempProduct: {}
     }
   },
   methods: {
@@ -45,6 +128,21 @@ export default {
     },
     hideModal () {
       this.modal.hide()
+    },
+    uploadFile () {
+      const uploadedFile = this.$refs.fileInput.files[0]
+      const formDate = new FormData()
+      formDate.append('file-to-upload', uploadedFile)
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/upload`
+      this.$http.post(url, formDate).then((res) => {
+        if (res.data.success) {
+          this.innerTempProduct.imageUrl = res.data.imageUrl
+        } else {
+          alert('請填寫所有必填欄位')
+          this.getProducts()
+          console.log(res)
+        }
+      })
     }
   },
   mounted () {
--- src/views/Productlist.vue
+++ src/views/Productlist.vue
@@ -10,7 +10,7 @@
             <div class="p-4">
             <div class="text-end mt-3">
                 <button class="btn btn-warning btn-sm" type="button"
-                @click="$refs.productModal.showModal()">
+                @click="openModal(true)">
                 增加產品
                 </button>
             </div>
@@ -37,14 +37,14 @@
                     </td>
                     <td class="text-center">
                         <div class="btn-group">
-                        <button class="btn btn-outline-primary btn-sm">編輯</button>
+                        <button class="btn btn-outline-primary btn-sm" @click="openModal(false, item)">編輯</button>
                         <button class="btn btn-outline-danger btn-sm">刪除</button>
                         </div>
                     </td>
                     </tr>
                 </tbody>
                 </table>
-                <ProductModal ref="productModal"></ProductModal>
+                <ProductModal ref="productModal" :modalProduct="tempProduct" @update-product="updateProduct"></ProductModal>
             </div>
           </main>
 </template>
@@ -56,13 +56,24 @@ export default {
   data () {
     return {
       products: [],
-      pagination: {}
+      pagination: {},
+      tempProduct: {},
+      isNew: false
     }
   },
   components: {
     ProductModal
   },
   methods: {
+    openModal (isNew, item) {
+      if (isNew) {
+        this.tempProduct = {}
+      } else {
+        this.tempProduct = { ...item }
+      }
+      this.isNew = isNew
+      this.$refs.productModal.showModal()
+    },
     getProducts () {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`
       this.$http.get(api).then((res) => {
@@ -70,6 +81,28 @@ export default {
           console.log(res.data)
           this.products = res.data.products
           this.pagination = res.data.pagination
+        }
+      })
+    },
+    updateProduct (item) {
+      this.tempProduct = item
+      // 更新
+      let api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
+      let httpMethod = 'post'
+      // 編輯
+      if (!this.isNew) {
+        api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${item.id}`
+        httpMethod = 'put'
+      }
+      const productComponet = this.$refs.productModal
+      this.$http[httpMethod](api, { data: this.tempProduct }).then((res) => {
+        if (res.data.success) {
+          productComponet.hideModal()
+          this.getProducts()
+        } else {
+          alert('請填寫所有必填欄位')
+          this.getProducts()
+          console.log(res)
         }
       })
     }
